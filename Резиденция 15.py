e9246057d1de784e9e0417301867fd6051c01088
Maxim <96412415+MaksimOlshanskiy@users.noreply.github.com>
2025-03-24 12:07:45 +0300

    Add files via upload

---
+++ Резиденция 15.py
@@ -0,0 +1,167 @@
+import requests
+import datetime
+import time
+import pandas as pd
+import openpyxl
+import os
+import random
+
+cookies = {
+    '_ym_uid': '1742543962742666359',
+    '_ym_d': '1742543962',
+    '_ym_isad': '2',
+}
+
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-GB;q=0.8,en;q=0.7,en-US;q=0.6',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Referer': 'https://domoplaner.ru/catalog/302/tCDzeZ/?start=1&domain=aHR0cHM6Ly9yZXNpZGVuY2UtMTUucnU%3D&back=1&state=plans',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Storage-Access': 'active',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    # 'Cookie': '_ym_uid=1742543962742666359; _ym_d=1742543962; _ym_isad=2',
+}
+
+flats = []
+
+
+def extract_digits_or_original(s):
+    digits = ''.join([char for char in s if char.isdigit()])
+    return int(digits) if digits else s
+
+
+response = requests.get('https://domoplaner.ru/widget-api/widget/302-tCDzeZ/', cookies=cookies, headers=headers)
+
+items = response.json()['flats']
+
+for i in items:
+
+    url = ""
+
+    date = datetime.date.today()
+    project = "Резиденция 15"
+    english = ''
+    promzona = ''
+    mestopolozhenie = ''
+    subway = ''
+    distance_to_subway = ''
+    time_to_subway = ''
+    mck = ''
+    distance_to_mck = ''
+    time_to_mck = ''
+    bkl = ''
+    distance_to_bkl = ''
+    time_to_bkl = ''
+    status = ''
+    start = ''
+    comment = ''
+    developer = "АРС"
+    okrug = ''
+    district = ''
+    adress = ''
+    eskrou = ''
+    korpus = extract_digits_or_original(i['house_title'])
+    konstruktiv = ''
+    klass = ''
+    srok_sdachi = ''
+    srok_sdachi_old = ''
+    stadia = ''
+    dogovor = ''
+    type = 'Апартаменты'
+    if i['decoration_id'] == 34:
+        finish_type = 'Предчистовая отделка'
+    elif i['decoration_id'] == 33:
+        finish_type = 'Без отделки'
+    else:
+        finish_type = i['decoration_id']
+    if i['is_studio'] == 1:
+        room_count = f'E-{i["rooms"]}'
+    else:
+        room_count = int(i["rooms"])
+    area = i["area"]
+    price_per_metr = ''
+    old_price = i['price']
+    discount = ''
+    price_per_metr_new = ''
+    price = i["price_with_discount"]
+    section = extract_digits_or_original(i["section_title"])
+    floor = i["floor_number"]
+    flat_number = i['number']
+
+    print(
+        f"{project}, {url}, отделка: {finish_type}, тип: {room_count}, площадь: {area}, цена: {price}, старая цена: {old_price}, корпус: {korpus}, этаж: {floor}")
+    result = [date, project, english, promzona, mestopolozhenie, subway, distance_to_subway, time_to_subway, mck,
+              distance_to_mck, time_to_mck, distance_to_bkl,
+              time_to_bkl, bkl, status, start, comment, developer, okrug, district, adress, eskrou, korpus, konstruktiv,
+              klass, srok_sdachi, srok_sdachi_old,
+              stadia, dogovor, type, finish_type, room_count, area, price_per_metr, old_price, discount,
+              price_per_metr_new, price, section, floor, flat_number]
+    flats.append(result)
+
+
+df = pd.DataFrame(flats, columns=['Дата обновления',
+                                  'Название проекта',
+                                  'на англ',
+                                  'промзона',
+                                  'Местоположение',
+                                  'Метро',
+                                  'Расстояние до метро, км',
+                                  'Время до метро, мин',
+                                  'МЦК/МЦД/БКЛ',
+                                  'Расстояние до МЦК/МЦД, км',
+                                  'Время до МЦК/МЦД, мин',
+                                  'БКЛ',
+                                  'Расстояние до БКЛ, км',
+                                  'Время до БКЛ, мин',
+                                  'статус',
+                                  'старт',
+                                  'Комментарий',
+                                  'Девелопер',
+                                  'Округ',
+                                  'Район',
+                                  'Адрес',
+                                  'Эскроу',
+                                  'Корпус',
+                                  'Конструктив',
+                                  'Класс',
+                                  'Срок сдачи',
+                                  'Старый срок сдачи',
+                                  'Стадия строительной готовности',
+                                  'Договор',
+                                  'Тип помещения',
+                                  'Отделка',
+                                  'Кол-во комнат',
+                                  'Площадь, кв.м',
+                                  'Цена кв.м, руб.',
+                                  'Цена лота, руб.',
+                                  'Скидка,%',
+                                  'Цена кв.м со ск, руб.',
+                                  'Цена лота со ск, руб.',
+                                  'секция',
+                                  'этаж',
+                                  'номер'])
+
+current_date = datetime.date.today()
+
+# Базовый путь для сохранения
+base_path = r"C:\PycharmProjects\SeleniumParcer\Арс"
+
+folder_path = os.path.join(base_path, str(current_date))
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+filename = f"{developer}_{current_date}.xlsx"
+
+# Полный путь к файлу
+file_path = os.path.join(folder_path, filename)
+
+# Сохранение файла в папку
+df.to_excel(file_path, index=False)

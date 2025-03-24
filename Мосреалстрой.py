950b85399b4f0381d1dd3acbdc85aee5eadb4f4d
Maxim <96412415+MaksimOlshanskiy@users.noreply.github.com>
2025-03-24 12:09:38 +0300

    Add files via upload

---
+++ Мосреалстрой.py
@@ -0,0 +1,159 @@
+import requests
+import datetime
+import time
+import pandas as pd
+import openpyxl
+import os
+import random
+
+headers = {
+    'accept': '*/*',
+    'accept-language': 'ru-RU,ru;q=0.9,en-GB;q=0.8,en;q=0.7,en-US;q=0.6',
+    'cache-control': 'no-cache',
+    'content-type': 'application/json',
+    'origin': 'https://mosrealstroy.ru',
+    'pragma': 'no-cache',
+    'priority': 'u=1, i',
+    'referer': 'https://mosrealstroy.ru/',
+    'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+}
+
+
+flats = []
+
+
+def extract_digits_or_original(s):
+    digits = ''.join([char for char in s if char.isdigit()])
+    return int(digits) if digits else s
+
+
+response = requests.get('https://hub.feedfox.ru/json/vzelenograde', headers=headers)
+
+items = response.json()
+
+for i in items:
+    if i['statuscode'] == '4' and i['subtypecode'] == '2' and i['typecode'] == '2':
+        url = ''
+
+        date = datetime.date.today()
+        project = "В Зеленограде"
+
+        english = ''
+        promzona = ''
+        mestopolozhenie = ''
+        subway = ''
+        distance_to_subway = ''
+        time_to_subway = ''
+        mck = ''
+        distance_to_mck = ''
+        time_to_mck = ''
+        bkl = ''
+        distance_to_bkl = ''
+        time_to_bkl = ''
+        status = ''
+        start = ''
+        comment = ''
+        developer = "Мосреалстрой"
+        okrug = ''
+        district = ''
+        adress = i['address']
+        eskrou = ''
+        korpus = ''
+        konstruktiv = ''
+        klass = ''
+        srok_sdachi = ''
+        srok_sdachi_old = ''
+        stadia = ''
+        dogovor = ''
+        type = 'Апартаменты'
+        finish_type = ''
+        room_count = int(i['rooms'])
+
+        area = float(i['square'])
+        price_per_metr = ''
+        try:
+            old_price = int(i['oldPrice'])
+        except:
+            old_price = ''
+        discount = ''
+        price_per_metr_new = ''
+        price = int(i["price"])
+        section = int(i['sectionNumber'])
+        floor = int(i['floor'])
+        flat_number = int(i['btiNumber'])
+
+        print(
+            f"{project}, {url}, отделка: {finish_type}, тип: {room_count}, площадь: {area}, цена: {price}, старая цена: {old_price}, корпус: {korpus}, этаж: {floor}")
+        result = [date, project, english, promzona, mestopolozhenie, subway, distance_to_subway, time_to_subway, mck,
+                  distance_to_mck, time_to_mck, distance_to_bkl,
+                  time_to_bkl, bkl, status, start, comment, developer, okrug, district, adress, eskrou, korpus, konstruktiv,
+                  klass, srok_sdachi, srok_sdachi_old,
+                  stadia, dogovor, type, finish_type, room_count, area, price_per_metr, old_price, discount,
+                  price_per_metr_new, price, section, floor, flat_number]
+        flats.append(result)
+
+
+df = pd.DataFrame(flats, columns=['Дата обновления',
+                                  'Название проекта',
+                                  'на англ',
+                                  'промзона',
+                                  'Местоположение',
+                                  'Метро',
+                                  'Расстояние до метро, км',
+                                  'Время до метро, мин',
+                                  'МЦК/МЦД/БКЛ',
+                                  'Расстояние до МЦК/МЦД, км',
+                                  'Время до МЦК/МЦД, мин',
+                                  'БКЛ',
+                                  'Расстояние до БКЛ, км',
+                                  'Время до БКЛ, мин',
+                                  'статус',
+                                  'старт',
+                                  'Комментарий',
+                                  'Девелопер',
+                                  'Округ',
+                                  'Район',
+                                  'Адрес',
+                                  'Эскроу',
+                                  'Корпус',
+                                  'Конструктив',
+                                  'Класс',
+                                  'Срок сдачи',
+                                  'Старый срок сдачи',
+                                  'Стадия строительной готовности',
+                                  'Договор',
+                                  'Тип помещения',
+                                  'Отделка',
+                                  'Кол-во комнат',
+                                  'Площадь, кв.м',
+                                  'Цена кв.м, руб.',
+                                  'Цена лота, руб.',
+                                  'Скидка,%',
+                                  'Цена кв.м со ск, руб.',
+                                  'Цена лота со ск, руб.',
+                                  'секция',
+                                  'этаж',
+                                  'номер'])
+
+current_date = datetime.date.today()
+
+# Базовый путь для сохранения
+base_path = r"C:\PycharmProjects\SeleniumParcer\Мосреалстрой"
+
+folder_path = os.path.join(base_path, str(current_date))
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+filename = f"{developer}_{project}_{current_date}.xlsx"
+
+# Полный путь к файлу
+file_path = os.path.join(folder_path, filename)
+
+# Сохранение файла в папку
+df.to_excel(file_path, index=False)
